20cd6347bb57e96178fd9ebff07f89338ec075e6
Luciano Bueno <luciano.bueno@ingacode.com.br>
2023-02-17 18:20:27 -0300

    bootstrapvuenext + sass working

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  modules: ["bootstrap-vue-next/nuxt"],
+  css: ["bootstrap/dist/css/bootstrap.css"],
+});
